2ca4480db000266864dc13543f533ddf18e1fcfc
x-val <x-val@x-val>
2020-04-17 10:55:05 +0300

    Project change to Maven

--- pr_1/proj_1/proj_1/src/ControllerPart/main.java
+++ pr_1/proj_1/proj_1/src/ControllerPart/main.java
@@ -1,13 +1,10 @@
 package ControllerPart;
-/*+++++++++++++++++++++++++++++
-++++++++++++++++++++++++++++
-+++++++++++++++
-*/
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class main {
-int b;
+
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 materials1_2 obj = new materials1_2();
