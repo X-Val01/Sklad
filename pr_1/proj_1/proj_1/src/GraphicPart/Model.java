26f255a2966c0ce282ffd3bd927d6e799da5a321
x-val <x-val@x-val>
2020-06-17 21:12:26 +0300

    version del controll part

--- pr_1/proj_1/proj_1/src/GraphicPart/Model.java
+++ pr_1/proj_1/proj_1/src/GraphicPart/Model.java
@@ -1,10 +1,8 @@
 package GraphicPart;
 
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,101 +10,101 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-import ControllerPart.ModelTech;
-
-public class Model   {
+public class Model {
 	private Object[][] iMasWord;
 	private Object[] iColumnHead;
 	private List<String> tablArray;
-	
+
 	public Model() {
-	
+
 		tablArray = new ArrayList<String>();
-		iMasWord= new Object[6][];
-		iColumnHead =new Object[6];
+		iMasWord = new Object[6][];
+		iColumnHead = new Object[6];
 	}
-	public Object[] getcolumnHead()  {
-		iColumnHead[0] = "N   ";	
-		iColumnHead[1] = "N company";	
-		iColumnHead[2] = "Name Material   ";	
-		iColumnHead[3] = " Quantity   ";	
-		iColumnHead[4] = " Price.   ";	
+
+	public Object[] getcolumnHead() {
+		iColumnHead[0] = "N   ";
+		iColumnHead[1] = "N company";
+		iColumnHead[2] = "Name Material   ";
+		iColumnHead[3] = " Quantity   ";
+		iColumnHead[4] = " Price.   ";
 		iColumnHead[5] = "Full Price";
 		return iColumnHead;
 	}
-	public List CarryOverInfoFileToList()  {
+
+	public List CarryOverInfoFileToList() {
 		BufferedReader reader;
-		try { reader = new BufferedReader(new FileReader(new File("/home/x-val/git/Sklad/Sklad/pr_1/ProgrammInfo.txt")));
-				String str;
-				tablArray.clear();
-				while ((str = reader.readLine()) != null) {
-					tablArray.add(str);
-					
-				}	
-		} catch ( Exception e) {
+		try {
+			reader = new BufferedReader(new FileReader(new File("/home/x-val/git/Sklad/Sklad/pr_1/ProgrammInfo.txt")));
+			String str;
+			tablArray.clear();
+			while ((str = reader.readLine()) != null) {
+				tablArray.add(str);
+
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
-		}	
-	 
+		}
+
 		return tablArray;
 	}
+
 	public Object[][] word() {
-		
-			tablArray = CarryOverInfoFileToList();
-		for (int i = 0; i < tablArray.size(); i++) {	
-			iMasWord[i]=	tablArray.get(i).split("\t\t");				
-			
-	}
+
+		tablArray = CarryOverInfoFileToList();
+		for (int i = 0; i < tablArray.size(); i++) {
+			iMasWord[i] = tablArray.get(i).split("\t\t");
+
+		}
 		return iMasWord;
-		
 
 	}
-	
+
 	public List getTablArray() {
 		return tablArray;
 	}
-	
-	
-	public synchronized  void   setTablArray(List<String> list)  {
-		
-		this.tablArray= list;
-		
+
+	public synchronized void setTablArray(List<String> list) {
+
+		this.tablArray = list;
+
 		PrintWriter file;
 		try {
-			file = new PrintWriter(new BufferedWriter(new FileWriter
-					("/home/x-val/git/Sklad/Sklad/pr_1/ProgrammInfo.txt")));
-		
-		file.println("");
-		
-		
-		
-		for (int o = 0; o < tablArray.size(); o++) {
-			
-			
-				file = new PrintWriter(new BufferedWriter(new FileWriter
-						("/home/x-val/git/Sklad/Sklad/pr_1/ProgrammInfo.txt", true)));
+			file = new PrintWriter(
+					new BufferedWriter(new FileWriter("/home/x-val/git/Sklad/Sklad/pr_1/ProgrammInfo.txt")));
+
+			file.println("");
+
+			for (int o = 0; o < tablArray.size(); o++) {
+
+				file = new PrintWriter(
+						new BufferedWriter(new FileWriter("/home/x-val/git/Sklad/Sklad/pr_1/ProgrammInfo.txt", true)));
 				file.println(tablArray.get(o));
 				file.close();
 			}
-		tablArray.clear();
+			tablArray.clear();
 		} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}			
-		}	
-	
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public int getSizeArray() {
 		return tablArray.size();
 	}
-	public void masword(){	
-		for (int i = 0; i < tablArray.size(); i++) {		
-			iMasWord[i]=	tablArray.get(i).split("\t\t");							
-	}		
+
+	public void masword() {
+		for (int i = 0; i < tablArray.size(); i++) {
+			iMasWord[i] = tablArray.get(i).split("\t\t");
 		}
-	public Object[][] getMasWord(){
+	}
+
+	public Object[][] getMasWord() {
 		return iMasWord;
 	}
+
 	public Object[] getColumnHead() {
 		return iColumnHead;
 	}
 
-	}
+}
--- pr_1/proj_1/proj_1/src/GraphicPart/View.java
+++ pr_1/proj_1/proj_1/src/GraphicPart/View.java
@@ -1,165 +1,131 @@
 package GraphicPart;
 
-
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import java.util.Scanner;
-
-
-
-
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 
 
-
-
-public class View    {
+public class View {
 	/**
 	 * 
 	 */
-	
+
 	private JButton buttonAdd;
 	private JButton buttonChange;
 	private JButton buttonDelete;
-	private JButton buttonOk;
-	private JButton buttonCancel;
-    public int lengthMas;
-    private String strFromTwoWind;
-    Object[] masWord;
-    Object[]masColumnHead;
-    Controller controller = new Controller();
-    WindowForInput windowInput = new WindowForInput();
-    Scanner scanner = new Scanner(System.in);
-    
+	public int lengthMas;
+	private String strFromTwoWind;
+	Object[] masWord;
+	Object[] masColumnHead;
+	Controller controller = new Controller();
+	WindowForInput windowInput = new WindowForInput();
+	
 
-    private JTable table;
-    public View() {
-    	JFrame frame = new JFrame("Table");
-    	strFromTwoWind = "";
+	private JTable table;
+
+	public View() {
+		JFrame frame = new JFrame("Table");
+		strFromTwoWind = "";
 		buttonAdd = new JButton("Add");
-    	buttonChange = new JButton("Change");
-    	buttonDelete = new JButton("Detele");
-    	buttonOk= new JButton("OK");
-		buttonCancel = new JButton("Cancel");				 
-		frame.setDefaultCloseOperation (frame.EXIT_ON_CLOSE);
-		//frame.setBounds(750,250,500,300);
+		buttonChange = new JButton("Change");
+		buttonDelete = new JButton("Detele");
+
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		frame.setPreferredSize(new Dimension(450, 200));
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 		table = new JTable(controller.buildNewTableModel());
 		table.setRowHeight(25);
-        table.setRowHeight(0, 20);
-       // table.setIntercellSpacing(new Dimension(10, 10));
-        table.setGridColor(Color.black);
-        JScrollPane scrollPane = new JScrollPane(table);
-
-		final JPanel panel = new JPanel();
-	    
-			
-			/*buttonAdd.setLocation(100, 225);
-			buttonChange.setLocation(200, 225);
-			buttonDelete.setLocation(300, 225);
-		    buttonAdd.setSize(100, 25);		    
-		    buttonChange.setSize(100, 25);
-		    buttonDelete.setSize(100, 25);*/
-		    frame. add(scrollPane,BorderLayout.SOUTH);
-		    
-		    panel.add(buttonAdd);
-		    
-		    panel.add(buttonChange);
-		    panel.add(buttonDelete);  
-		    frame.add(panel, BorderLayout.NORTH);
-		    frame.getContentPane().add(scrollPane);
-		    
-	          
+		table.setRowHeight(0, 20);
+		table.setGridColor(Color.black);
 		
-buttonAdd.addActionListener(new ActionListener() {
-			
+		JScrollPane scrollPane = new JScrollPane(table);
+		JPanel panel = new JPanel();
+
+		frame.add(scrollPane, BorderLayout.SOUTH);
+		panel.add(buttonAdd);
+		panel.add(buttonChange);
+		panel.add(buttonDelete);
+		frame.add(panel, BorderLayout.NORTH);
+		frame.getContentPane().add(scrollPane);
+
+		buttonAdd.addActionListener(new ActionListener() {
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 
 				strFromTwoWind = windowInput.newStringInfo("add");
-			controller.saveStringInList(strFromTwoWind);
-			controller.buildNewTableModel();
-			 frame.setVisible(false);
-			new View();
-		
-			}});
-			
-buttonDelete.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				int numberDelStr =0;
-				try {
-				strFromTwoWind = windowInput.newStringInfo("del");
-				 numberDelStr = Integer.parseInt(strFromTwoWind);				
-				if(0<numberDelStr) {					
-				controller.deleteEventButton(numberDelStr-1);
-				}else {
-					actionPerformed(arg0);
-				}
-				}catch ( Exception e) {
-					actionPerformed(arg0);	
-				}
+				controller.saveStringInList(strFromTwoWind);
 				controller.buildNewTableModel();
-				 frame.setVisible(false);
+				frame.setVisible(false);
 				new View();
 
-				
 			}
 		});
-buttonChange.addActionListener(new ActionListener() {
-	
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		int numberDelStr =0;
-		try {
-		strFromTwoWind = windowInput.newStringInfo("del");
-		 numberDelStr = Integer.parseInt(strFromTwoWind);				
-		if(0<numberDelStr) {					
-			controller.changeEventButton(numberDelStr-1);
-		}else {
-			actionPerformed(arg0);
-		}
-		}catch ( Exception e) {
-			actionPerformed(arg0);	
-		}
-		controller.buildNewTableModel();
-		 frame.setVisible(false);
-		new View();	
-	
-		
+
+		buttonDelete.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				int numberDelStr = 0;
+				try {
+					strFromTwoWind = windowInput.newStringInfo("del");
+					numberDelStr = Integer.parseInt(strFromTwoWind);
+					if (0 < numberDelStr) {
+						controller.deleteEventButton(numberDelStr - 1);
+					} else {
+						actionPerformed(arg0);
+					}
+				} catch (Exception e) {
+					actionPerformed(arg0);
+				}
+				controller.buildNewTableModel();
+				frame.setVisible(false);
+				new View();
+
+			}
+		});
+		buttonChange.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				int numberDelStr = 0;
+				try {
+					strFromTwoWind = windowInput.newStringInfo("del");
+					numberDelStr = Integer.parseInt(strFromTwoWind);
+					if (0 < numberDelStr) {
+						controller.changeEventButton(numberDelStr - 1);
+					} else {
+						actionPerformed(arg0);
+					}
+				} catch (Exception e) {
+					actionPerformed(arg0);
+				}
+				controller.buildNewTableModel();
+				frame.setVisible(false);
+				new View();
+
+			}
+		});
+
+		frame.setVisible(true);
+
 	}
-});
-			
-				
-frame.setVisible(true);
 
-	
-
-    }  
-   
-    public String getStrFromTwoWind() {
+	public String getStrFromTwoWind() {
 		return strFromTwoWind;
 	}
+
 	public void setStrFromTwoWind(String strFromTwoWind) {
 		this.strFromTwoWind = strFromTwoWind;
 	}
-    
- 
-    public static void main(String[] args) {	
-    	new View();
-    		
-    		
 
-    		
-    	
-    	}
-	
+	public static void main(String[] args) {
+		new View();
+
+	}
+
 }
